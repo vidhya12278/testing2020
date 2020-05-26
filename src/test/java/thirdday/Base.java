3575ef29553c64b6f0b32aee404392be31002bf8
vidhya12278 <evidhya60@gmail.com>
2020-05-26 10:16:06 +0530

    implemented

---
+++ src/test/java/thirdday/Base.java
@@ -0,0 +1,19 @@
+package thirdday;
+
+public class Base {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=19;
+		int c;
+		c=a+b;
+		System.out.println("add value:  " + c);
+		System.out.println("print product title");
+		System.out.println("print product price");
+		System.out.println("view");
+	
+		
+		
+	}
+}
